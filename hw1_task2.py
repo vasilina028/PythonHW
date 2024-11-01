5e44b8c74fefd54157aec89c7f6832f8702c9034
vasilina028 <vasilina028@gmail.com>
2024-11-01 19:39:35 +0300

    hw1_task2.py

--- hw1_task2.py
+++ hw1_task2.py
@@ -1,13 +1,5 @@
-a = input()
-d = {')': '(',
-     ']': '[',
-     '}': '{'}
-d1 = []
-for i in a:
-    if i in d.values():
-        d1.append(i)
-    elif i in d.keys():
-        if d1 == [] or d[i] != d1.pop():
-            print('False')
-            break
-print('True')
+line = input()
+if line == line[::-1]:
+    print('Палиндром')
+else:
+    print('Не палиндром')
