67044c48b63f453893c1440323270ac81014a032
vasilina028 <vasilina028@gmail.com>
2024-12-19 23:48:40 +0300

    Update hw1_task2.py

--- hw1_task2.py
+++ hw1_task2.py
@@ -1,5 +1,24 @@
-line = input()
-if line == line[::-1]:
-    print('Палиндром')
-else:
-    print('Не палиндром')
+def skobki(s):
+    stack = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+
+    for char in s:
+        if char in pairs.values():
+            stack.append(char)
+        elif char in pairs.keys():
+            if not stack or stack.pop() != pairs[char]:
+                return False
+
+    return len(stack) == 0
+
+test = [
+    "(({[]}))",  # True
+    "{(})",      # False
+    '([])[[]]',  # True
+    ")())[[]]",   # False
+    "(())[[]]",   # True
+    '([])'       
+]
+
+for case in test:
+    print(f"{case}: {skobki(case)}")
